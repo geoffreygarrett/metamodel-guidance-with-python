00d533ea9c0f9ad26d74d4eacddf2718f90eee8e
ggarrett13 <g.h.garrett13@gmail.com>
2019-09-13 15:01:47 +0200

    Merge branch 'master' of https://github.com/ggarrett13/NB10422645

--- src/surrogate/modules/_core.py
+++ src/surrogate/modules/_core.py
@@ -5,7 +5,8 @@ activation_dict = nn.ModuleDict([
     ['relu', nn.ReLU(inplace=True)],
     ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
     ['selu', nn.SELU(inplace=True)],
-    ['none', nn.Identity()]
+    ['none', nn.Identity()],
+    ['sig', nn.Sigmoid()]
 ])
 
 
