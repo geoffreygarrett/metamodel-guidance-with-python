a99303d65a7fad188b4e2406e7a12deb88ae3223
ggarrett13 <g.h.garrett13@gmail.com>
2019-12-03 16:57:49 +0100

    refactor(.ipynb_checkpoints): Deleted.

--- mmgpy/__init__.py
+++ mmgpy/__init__.py
@@ -1,5 +1,8 @@
 import os
 
+# TODO: Fix code detection on github with github-linguist. Over reporting of
+#  jupyter.
+
 DATA_DIR = os.path.join(os.getcwd(), "../data")
 TRAINED_MODELS_DIR = os.path.join(os.getcwd(), "../models")
 SIMULATION_DIR = os.path.join(os.getcwd(), "../simulation")
